71df71cb7f33f38bbd4e71a1a1dac6fdac6bed81
John Strunk <john.strunk@gmail.com>
2023-07-16 11:36:23 -0400

    Fix property substitution... again
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- scripts/configure-server.sh
+++ scripts/configure-server.sh
@@ -2,7 +2,8 @@
 
 # Create a list of all environment variables starting with "MC_"
 # and update the server.properties file accordingly
-echo "Updating server.properties..."
+PROP_FILE=server.properties
+echo "Updating $PROP_FILE..."
 for var in "${!MC_@}"; do
     # Remove the "MC_" prefix
     name=${var:3}      # Remove the "MC_" prefix
@@ -10,8 +11,8 @@ for var in "${!MC_@}"; do
     value=${!var}
     echo "Setting '$name' to '$value'"
     # https://stackoverflow.com/a/66949954
-    if ! sed --quiet -i "s/^$name.*/$name=$value/;t1;b2;:1;h;:2;p;\${g;s/.+//;tok;q1;:ok}" server.properties; then
+    if ! sed --quiet -i "s/^$name.*/$name=$value/;t1;b2;:1;h;:2;p;\${g;s/..*//;tok;q1;:ok}" "$PROP_FILE"; then
         echo "$name wasn't found in the file... adding"
-        echo "$name=$value" >> server.properties
+        echo "$name=$value" >> "$PROP_FILE"
     fi
 done
