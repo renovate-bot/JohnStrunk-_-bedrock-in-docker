298b058fe7ad458458f25b5770eb4027e3dda1c5
John Strunk <john.strunk@gmail.com>
2024-06-09 22:49:45 +0000

    Forget old restic backups
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,4 +6,12 @@ if [[ -d "/bedrock/worlds" &&  -n $RESTIC_REPOSITORY ]]; then
     /bedrock/server.properties \
     /bedrock/permissions.json \
     /bedrock/allowlist.json
+
+  echo "Forget old backups"
+  restic forget --cleanup-cache \
+    --keep-daily 14 \
+    --keep-weekly 4 \
+    --keep-monthly 12 \
+    --keep-yearly 2 \
+    --keep-tag keep
 fi
